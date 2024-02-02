b410e09fae0e10ea1fa11d48fe84f5c035e69786
RobinKD <robin.kd@protonmail.com>
2024-02-02 09:59:00 +0100

    Change service call on timer to call on path changed
    Makes it much easier and more robust
    Also can integrate with remove passwords

--- hosts/shared/custom-services/backup-passwords.nix
+++ hosts/shared/custom-services/backup-passwords.nix
@@ -5,27 +5,25 @@ let
   homeDir = hm.home.homeDirectory;
   hostname = config.networking.hostName;
 in {
-  systemd.timers."backup-passwords" = {
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnBootSec = "5m";
-      OnUnitActiveSec = "6h";
+  systemd.paths."backup-passwords" = {
+    pathConfig = {
+      PathChanged = "${homeDir}/.password-store";
       Unit = "backup-passwords.service";
     };
+    wantedBy = [ "multi-user.target" ];
   };
 
   systemd.services."backup-passwords" = {
     path = [ pkgs.gnutar pkgs.gzip pkgs.gnupg pkgs.sops ];
     script = ''
       bkp_file="${homeDir}/.dotfiles/secrets/pass_bkp.tar.gz"
-      if [ ! -f $bkp_file ] || [ $(find ${homeDir}/.password-store/ -type f -newer $bkp_file | wc -l) -gt 0 ]; then
-         tar czf ${homeDir}/.dotfiles/secrets/pass_bkp.tar.gz ${homeDir}/.password-store/
-         sops --config ${homeDir}/.dotfiles/.sops.yaml -e $bkp_file > $bkp_file
-      fi
+      tar czf $bkp_file ${homeDir}/.password-store/
+      sops --config ${homeDir}/.dotfiles/.sops.yaml -e $bkp_file > $bkp_file
     '';
     serviceConfig = {
       Type = "simple";
       User = "${username}";
     };
+    wantedBy = [ "multi-user.target" ];
   };
 }
