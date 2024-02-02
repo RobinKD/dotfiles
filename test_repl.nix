d1a7da9886ff1e587f5d79fc78c6499fc6bad495
RobinKD <robin.kd@protonmail.com>
2024-02-02 11:10:13 +0100

    Add test_repl file
    Allows to easily test a file's configuration before rebuild

---
+++ test_repl.nix
@@ -0,0 +1,24 @@
+/* To use, go to flake repo, then put your host and username in the defined vars
+   Import has to be the file you want to test
+   Run nix repl --file test_repl.nix
+   The result is available in nix repl with :p test
+*/
+let
+  host = "aquila";
+  username = "keanu";
+  nixos = builtins.getFlake "/home/${username}/.dotfiles/"; # or your own path
+in rec {
+  inherit nixos;
+  inherit (nixos) inputs nixosConfigurations;
+  lib = inputs.nixpkgs.lib;
+  pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+  config = nixosConfigurations."${host}".config;
+  homeConfig = config.home-manager.users."${username}";
+  options = nixosConfigurations."${host}".options;
+  /* Replace inherit config by config = homeConfig if file is called
+     from home.nix
+  */
+  test = import ./home/shared/git/replacements.nix.expunged {
+    inherit lib pkgs config options;
+  };
+}
