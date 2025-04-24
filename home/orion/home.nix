ab2da879297dab8398c564605d2315f3e88e70cc
RobinKD <robin.kd@protonmail.com>
2025-04-24 11:28:57 +0200

    Hyprland config: Add screen refresh rate

--- home/orion/home.nix
+++ home/orion/home.nix
@@ -65,7 +65,7 @@ rec {
   systemd.user.startServices = "sd-switch";
 
   wayland.windowManager.hyprland.settings.monitor = lib.mkIf hm-modules.hyprland.enable [
-    "desc:Beihai Century Joint Innovation Technology Co.Ltd QMC-VA30-02 0000000000000,2560x1080,0x0,1"
+    "desc:Beihai Century Joint Innovation Technology Co.Ltd QMC-VA30-02 0000000000000,2560x1080@165,0x0,1"
     "desc:Samsung Electric Company C27F390 H4LR605458,1920x1080@60,800x-1080,1"
     "desc:Dell Inc. DELL S2425H FZM8M04,1920x1080@60,-1120x-1080,1"
     "desc:ASUSTek COMPUTER INC ASUS VA24EQSB S9LMTF185712,1920x1080@60,-1080x0,1,transform,1"
