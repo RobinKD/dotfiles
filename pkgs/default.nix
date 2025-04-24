632492d6cf0a2c60ac0c18a3d6c94d64e35a441c
RobinKD <robin.kd@protonmail.com>
2025-04-24 11:26:55 +0200

    Add custom ib-tws package
    Inspired from NUR

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,11 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
 
-{ pkgs ? (import ../nixpkgs.nix) { } }: {
+{
+  pkgs ? (import ../nixpkgs.nix) { },
+}:
+{
   sddm-sugar-dark-theme = pkgs.callPackage ./sddm-sugar-dark { };
   sddm-sugar-candy-theme = pkgs.callPackage ./sddm-sugar-candy { };
+  ib-tws = pkgs.callPackage ./ib-tws { };
 }
