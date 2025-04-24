d77ca86d268eff60279fc7b6a7e04faf844e7929
RobinKD <robin.kd@protonmail.com>
2025-04-24 11:28:01 +0200

    nix-fmt

--- hosts/shared/packages.nix
+++ hosts/shared/packages.nix
@@ -1,49 +1,57 @@
-{ inputs, pkgs, ... }:
+{
+  inputs,
+  outputs,
+  pkgs,
+  ...
+}:
 let
   myEmacs = (import ./emacs.nix { inherit pkgs; });
 in
-with pkgs;
 {
   services.emacs = {
     enable = true;
     package = myEmacs.packaged-emacs; # replace with emacs-gtk, or a version provided by the community overlay if desired.
   };
-  environment.systemPackages = [
-    # Basic
-    git
-    ntfs3g
-    wget
-    ripgrep
-    gcc
-    alacritty
-    pavucontrol
-    brightnessctl
-    networkmanagerapplet
+  environment.systemPackages =
+    with pkgs;
+    [
+      # Basic
+      git
+      ntfs3g
+      wget
+      ripgrep
+      gcc
+      alacritty
+      pavucontrol
+      brightnessctl
+      networkmanagerapplet
 
-    # Always useful
-    librewolf
-    gparted
-    gpg-tui
-    featherpad
-    libreoffice
-    nomacs
-    nemo
-    libnotify
-    cmake
-    gnumake
-    libtool
-    sops
-    age
-    eza
-    fzf
-    ripgrep-all
-    inputs.nix-alien.packages.${pkgs.system}.nix-alien
+      # Always useful
+      librewolf
+      gparted
+      gpg-tui
+      featherpad
+      libreoffice
+      nomacs
+      nemo
+      libnotify
+      cmake
+      gnumake
+      libtool
+      sops
+      age
+      eza
+      fzf
+      ripgrep-all
+      inputs.nix-alien.packages.${pkgs.system}.nix-alien
 
-    # Trading
-    qemu
-    quickemu
-    tradingview
-  ] ++ myEmacs.system_packages;
+      # Trading
+      qemu
+      quickemu
+      tradingview
+      ib-tws
+    ]
+    ++ myEmacs.system_packages;
 
   programs.nix-ld.enable = true;
 
