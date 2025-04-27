8a2082e6aba9f322e2be1df928a3744d20f993fa
RobinKD <robin.kd@protonmail.com>
2025-04-27 21:20:14 +0200

    nixfmt

--- hosts/shared/custom-services/backup-passwords.nix
+++ hosts/shared/custom-services/backup-passwords.nix
@@ -1,11 +1,17 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   hm = config.home-manager.users.keanu;
   username = hm.home.username;
   homeDir = hm.home.homeDirectory;
   hostname = config.networking.hostName;
   bkp_file = "${homeDir}/.dotfiles/secrets/pass_bkp";
-in {
+in
+{
   systemd.timers."backup-passwords" = {
     wantedBy = [ "timers.target" ];
     timerConfig = {
@@ -16,7 +22,12 @@ in {
   };
 
   systemd.services."backup-passwords" = {
-    path = [ pkgs.gnutar pkgs.gzip pkgs.gnupg pkgs.sops ];
+    path = [
+      pkgs.gnutar
+      pkgs.gzip
+      pkgs.gnupg
+      pkgs.sops
+    ];
     script = ''
       if [ ! -f ${bkp_file}.tar.gz ] || [ $(find ${homeDir}/.password-store/ -type f -newer ${bkp_file}.tar.gz | wc -l) -gt 0 ] || [ $(stat --format "%Z" ${homeDir}/.password-store) -gt $(stat --format "%Z" ${bkp_file}.tar.gz) ]; then
          tar czf ${bkp_file}2.tar.gz ${homeDir}/.password-store/
