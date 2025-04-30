ae8c1d68c7533f25e1cf40c7f40afc293dfcd200
RobinKD <robin.kd@protonmail.com>
2025-04-30 14:13:33 +0200

    Rename pinentryPackage
    Follow nixpkgs convention

--- home/shared/gpg/default.nix
+++ home/shared/gpg/default.nix
@@ -1,10 +1,22 @@
-{ config, lib, pkgs, ... }:
-let cfg = config.hm-modules.gpg;
-in with lib; {
-  options.hm-modules.gpg = { enable = mkEnableOption "gpg"; };
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.hm-modules.gpg;
+in
+with lib;
+{
+  options.hm-modules.gpg = {
+    enable = mkEnableOption "gpg";
+  };
 
   config = mkIf cfg.enable {
-    programs.gpg = { enable = true; };
+    programs.gpg = {
+      enable = true;
+    };
 
     home.packages = with pkgs; [ pinentry-all ];
     services.gpg-agent = {
@@ -12,7 +24,7 @@ in with lib; {
       enableBashIntegration = true;
       defaultCacheTtl = 25200; # 7h
       maxCacheTtl = 86400; # 24h
-      pinentryPackage = pkgs.pinentry-gtk2;
-     };
+      pinentry.package = pkgs.pinentry-gtk2;
+    };
   };
 }
