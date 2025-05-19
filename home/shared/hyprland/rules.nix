76df546a04f4a476b11408926634daf1716842ba
RobinKD <robin.kd@protonmail.com>
2025-05-19 14:52:04 +0200

    Hyprland: Add signal & telegram window rules

--- home/shared/hyprland/rules.nix
+++ home/shared/hyprland/rules.nix
@@ -7,14 +7,14 @@
       "opacity 0.97 override 0.93 override,title:^(.*Emacs.*|.*Alacritty.*)$"
       "opacity 0.97 override 0.93 override,class:^(Alacritty)$"
 
-      "maximize,class:^(firefox|librewolf|WebCord|Element)$"
+      "maximize,class:^(firefox|librewolf|WebCord|Element|org.telegram.desktop|signal)$"
       "maximize,title:^(.*Mint-XFCE.*)"
       "float, title:^(Picture-in-Picture)$"
       "pin, title:^(Picture-in-Picture)$"
       "float, title:^(alacritty_float)$"
       "float, title:^(kitty_float)$"
 
-      "workspace name:Social silent,class:^(WebCord|Element)$"
+      "workspace name:Social silent,class:^(WebCord|Element|org.telegram.desktop|signal)$"
       "workspace name:Web silent,class:^(firefox|librewolf)$"
       "workspace name:Mail silent,title:^(.*mu4e.*)"
       "workspace name:Scanners silent,title:^(.*Screener.*)"
