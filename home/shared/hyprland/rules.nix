284ac530377b51a9cd1f2a33efec1658a6968d9a
RobinKD <robin.kd@protonmail.com>
2025-06-18 11:33:53 +0200

    Remove scanner workspace

--- home/shared/hyprland/rules.nix
+++ home/shared/hyprland/rules.nix
@@ -17,14 +17,13 @@
       "workspace name:Social silent,class:^(WebCord|Element|org.telegram.desktop|signal)$"
       "workspace name:Web silent,class:^(firefox|librewolf)$"
       "workspace name:Mail silent,title:^(.*mu4e.*)"
-      "workspace name:Scanners silent,title:^(.*Screener.*)"
-      "workspace name:Charts silent,title:^(.*Chart.*|.*chart.*)"
       "workspace name:Action silent,class:^(install4j-jclient-LoginFrame)$"
+      "workspace name:Charts silent,class:TradingView"
     ];
 
     # Workspace binding
     workspace = [
-      "name:Scanners,monitor:desc:Dell Inc. DELL S2425H FZM8M04,default:true"
+      # "name:Scanners,monitor:desc:Dell Inc. DELL S2425H FZM8M04,default:true"
     ];
   };
 }
