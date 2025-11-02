d3955a9df9802ff0eb5079e9dfede2fd10af999f
RobinKD <robin.kd@protonmail.com>
2025-11-02 20:05:36 +0100

    Add Gemini and Grok to desktop items

--- hosts/shared/custom-desktop-items.nix
+++ hosts/shared/custom-desktop-items.nix
@@ -1,22 +1,52 @@
 { config, pkgs, ... }:
 let
   chatGPT-icon = pkgs.fetchurl {
-    url = "https://cdn3.iconfinder.com/data/icons/modern-future-technology/128/chat-gpt-512.png";
-    sha256 = "sha256-qPueVhne6TAFzC9PG7xPnO4+Pj6wszPY/45UpZmrheY=";
+    url = "https://logovent.com/blog/wp-content/uploads/2024/09/Chat-GPT-icon-2022-%E2%80%93-Today.png";
+    sha256 = "sha256-ECzbVDVN415+sW6B16mqTqjoUe9e+CFx9e0SPefIdFY=";
   };
 
   chatgpt = pkgs.makeDesktopItem {
     name = "ChatGPT";
     desktopName = "ChatGPT";
     comment = "Open chatgpt in Tor";
-    exec = "tor-browser --new-tab chatgpt.com";
+    exec = "librewolf  --new-tab chatgpt.com";
     icon = chatGPT-icon;
+    terminal = false;
+  };
+
+  grok-icon = pkgs.fetchurl {
+    url = "https://1000logos.net/wp-content/uploads/2025/02/Grok-Logo.png";
+    sha256 = "sha256-tk0mopw2IHyJPGk7Yx5/wQgoslrvf+UFMUkQG9OGcy4=";
+  };
+
+  grok = pkgs.makeDesktopItem {
+    name = "Grok";
+    desktopName = "Grok";
+    comment = "Open Grok in Librewolf";
+    exec = "librewolf --new-tab grok.com";
+    icon = grok-icon;
+    terminal = false;
+  };
+
+  gemini-icon = pkgs.fetchurl {
+    url = "https://www.google.com/s2/favicons?domain=gemini.google.com";
+    sha256 = "sha256-VB/3AbMdDpxRE7JjFTHGPBXHLuYcNDVsOv118bkQCzw=";
+  };
+
+  gemini = pkgs.makeDesktopItem {
+    name = "Gemini";
+    desktopName = "Gemini";
+    comment = "Open Gemini in Librewolf";
+    exec = "librewolf --new-tab gemini.google.com";
+    icon = gemini-icon;
     terminal = false;
   };
 in
 {
   environment.systemPackages = with pkgs; [
     chatgpt
+    grok
+    gemini
   ];
 
 }
