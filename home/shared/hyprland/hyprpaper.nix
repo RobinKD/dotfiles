b751a71fa72769bf260530394dd87234aaec2080
RobinKD <robin.kd@protonmail.com>
2026-01-29 11:38:15 +0100

    Remove moved code

--- home/shared/hyprland/hyprpaper.nix
+++ home/shared/hyprland/hyprpaper.nix
@@ -5,14 +5,6 @@
   wallpapers,
   ...
 }:
-let
-  hyprlock-random = pkgs.writeShellScriptBin "hyprlock-random" ''
-    ${pkgs.coreutils}/bin/cp -f `${pkgs.findutils}/bin/find ${wallpapers} -type f | ${pkgs.coreutils}/bin/shuf -n 1` ${config.xdg.configHome}/hypr/background.jpg
-    wait &&
-    hyprlock
-  '';
-  hyprlock-package = inputs.hyprlock.packages.${pkgs.stdenv.hostPlatform.system}.hyprlock;
-in
 {
   services.hyprpaper = {
     enable = true;
