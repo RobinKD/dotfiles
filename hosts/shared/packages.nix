591789608aead33f792e00f5357bec527084ffd7
RobinKD <robin.kd@protonmail.com>
2026-03-08 00:31:19 +0100

    Remove qemu hypervisor patch

--- hosts/shared/packages.nix
+++ hosts/shared/packages.nix
@@ -47,23 +47,7 @@
 
     # Gaming
     lutris
-    (qemu.overrideAttrs (oldAttrs: {
-      version = "9.2.4";
-      src = pkgs.fetchurl {
-        url = "https://download.qemu.org/qemu-9.2.4.tar.xz";
-        sha256 = "sha256-88wcTqv9soghisPjN2Pb6eJ22LyJC4Z6IzXVjeLd05o=";
-      };
-      patches = (oldAttrs.patches or [ ]) ++ [
-        (pkgs.fetchurl {
-          url = "https://github.com/Scrut1ny/Hypervisor-Phantom/raw/refs/heads/main/Hypervisor-Phantom/patches/QEMU/intel-qemu-9.2.4.patch"; # change with amd, if you have amd cpu
-          sha256 = "sha256-BV6TvEtY0EnGJuTSWB128FGGDdjiqmYXKKnfwIVtzYw="; # Replace with actual hash
-        })
-        (pkgs.fetchurl {
-          url = "https://github.com/Scrut1ny/Hypervisor-Phantom/raw/refs/heads/main/Hypervisor-Phantom/patches/QEMU/libnfs6-qemu-9.2.4.patch";
-          sha256 = "sha256-HjZbgwWf7oOyvhJ4WKFQ996e9+3nVAjTPSzJfyTdF+4="; # Replace with actual hash
-        })
-      ];
-    }))
+    qemu
     quickemu
 
     # LLMs
