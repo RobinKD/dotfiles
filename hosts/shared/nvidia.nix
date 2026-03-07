aa8d8f068d22c52da10b641473b187909df4428c
RobinKD <robin.kd@protonmail.com>
2026-03-08 00:33:09 +0100

    Fix: Add nvidia patch
    To remove when added to unstable

--- hosts/shared/nvidia.nix
+++ hosts/shared/nvidia.nix
@@ -1,11 +1,26 @@
-{ config, ... } :
+{ config, pkgs, ... }:
 {
   hardware = {
     graphics = {
       enable = true;
     };
     nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      # package = config.boot.kernelPackages.nvidiaPackages.latest;
+      # TODO: remove once NVIDIA 580+ supports Linux 6.19 natively (upstream fix pending).
+      package =
+        let
+          base = config.boot.kernelPackages.nvidiaPackages.latest;
+          cachyos-nvidia-patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
+            sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
+          };
+        in
+        base
+        // {
+          open = base.open.overrideAttrs (oldAttrs: {
+            patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
+          });
+        };
       open = true;
       modesetting.enable = true;
 
