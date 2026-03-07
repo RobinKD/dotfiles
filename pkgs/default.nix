6514a96b6f5361c1b7cf3bd65a5b974826762209
RobinKD <robin.kd@protonmail.com>
2026-03-08 00:32:56 +0100

    Add new packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,8 +4,25 @@
 {
   pkgs ? (import ../nixpkgs.nix) { },
 }:
+with pkgs;
 {
   sddm-sugar-dark-theme = pkgs.callPackage ./sddm-sugar-dark { };
   sddm-sugar-candy-theme = pkgs.callPackage ./sddm-sugar-candy { };
   ib-tws = pkgs.callPackage ./ib-tws { };
+  # world-monitor = pkgs.callPackage ./world-monitor {
+  #   inherit
+  #     lib
+  #     appimageTools
+  #     fetchurl
+  #     pkgs
+  #     ;
+  # };
+  loa-logs = pkgs.callPackage ./loa-logs {
+    inherit
+      lib
+      appimageTools
+      pkgs
+      ;
+  };
+
 }
