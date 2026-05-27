b7e127527a2bfedf2f551b9b5917ae7a3607c9cb
RobinKD <robin.kd@protonmail.com>
2026-05-27 22:19:08 +0200

    Formatting

--- home/shared/pass/default.nix
+++ home/shared/pass/default.nix
@@ -1,14 +1,25 @@
-{ pkgs, config, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 let
   cfg = config.hm-modules.pass;
   homeDir = config.home.homeDirectory;
-in with lib; {
-  options.hm-modules.pass = { enable = mkEnableOption "pass"; };
+in
+with lib;
+{
+  options.hm-modules.pass = {
+    enable = mkEnableOption "pass";
+  };
 
   config = mkIf cfg.enable {
     programs.password-store = {
       enable = true;
-      settings = { PASSWORD_STORE_DIR = "${homeDir}/.password-store"; };
+      settings = {
+        PASSWORD_STORE_DIR = "${homeDir}/.password-store";
+      };
     };
 
     programs.browserpass = {
